e59e1187f64cbaa6867575a52ea36a2b36c1256a
hcy <18322002121@163.com>
2018-02-01 11:23:27 +0800

    初始化

--- Antwice/AppDelegate.m
+++ Antwice/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window=[[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    self.window.backgroundColor=[UIColor whiteColor];
+    RootViewController *rootView=[[RootViewController alloc]init];
+    UINavigationController *rootNav=[[UINavigationController alloc]initWithRootViewController:rootView];
+    self.window.rootViewController=rootNav;
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
--- Antwice/RootViewController.h
+++ Antwice/RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  Antwice
 //
 //  Created by hcy on 2018/2/1.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-
